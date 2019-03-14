74e715697a76f2eb007b6efb4456aa6e9ae3a45d
Sorin Dragomir <sorinel.dragomir@ownzones.com>
2019-03-14 09:40:11 +0200

    101

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AddExpense from './components/AddExpense/AddExpense';
 import EditExpense from './components/Edit/EditExpense';
 import NotFound from './components/NotFound/NotFound';
 import Header from './components/Header/Header';
+import Hoc from './playground/hoc'
 import './App.css';
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
         <Switch>
           <Route path="/" exact component={Dashboard} />
           <Route path="/create" component={AddExpense} />
+          <Route path="/hoc" component={Hoc} />
           <Route path="/edit/:id" component={EditExpense} />
           <Route component={NotFound} />
         </Switch>
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import ExpensesList from '../ExpensesList/ExpensesList';
 import ExpensesListFilters from '../ExpensesList/ExpensesListFilters/ExpensesListFilters';
+import Hoc from '../../playground/hoc';
 
 class Dashboard extends Component {
   render() {
@@ -10,6 +11,7 @@ class Dashboard extends Component {
         Dashboard
         <ExpensesListFilters />
         <ExpensesList />
+        <Hoc isAdmin={false} info="There are the details" />
       </div>
     );
   }
