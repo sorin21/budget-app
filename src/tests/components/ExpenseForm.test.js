2159c3aeb93da306a20f05c22e822e0949299f07
Sorin <sorin21us@yahoo.com>
2019-04-01 20:12:09 +0300

    Error: Please provide description and amount

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -13,3 +13,18 @@ test('should render ExpenseForm with an expense', () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('should render error for invalid form submission', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+
+  // simulate submitting the form
+  // could be click, change
+  // we choose submit because form has onSubmit event
+  // we can simulate the event from event.preventDefault()
+  // so the 2nd art, the obj, is the event
+  wrapper.find('form').simulate('submit', { preventDefault: () => { } })
+  // if it is zero the test will fail
+  expect(wrapper.state('error').length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
+});
